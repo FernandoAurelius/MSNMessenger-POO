79be8f9a2ea86c9819d4e9ca4ae8eca0b27b79b0
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-09-23 11:05:51 -0300

    refactor(message): improve code structure with inheritance and abstraction
    This commit refactors the message package to improve code structure by introducing inheritance and abstraction.
    
    - The `InstantMessageService` class is now an abstract class, defining the common methods `enviarMensagem` and `receberMensagem`.
    - The concrete classes `FacebookMessenger`, `MSNMessenger`, and `Telegram` now extend `InstantMessageService` and implement the abstract methods.
    - This improves code reusability and maintainability, as common functionality is now defined in the abstract class.
    - The code is also more concise and easier to understand.

--- src/message/FacebookMessenger.java
+++ src/message/FacebookMessenger.java
@@ -1,8 +1,15 @@
 package message;
 
-// Classe melhorada com herança
+// Classe melhorada com herança e abstração!
 public class FacebookMessenger extends InstantMessageService {
-    public FacebookMessenger() {
-        super("Facebook");
+
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Facebook!");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook!");
     }
 }
--- src/message/InstantMessageService.java
+++ src/message/InstantMessageService.java
@@ -1,30 +1,7 @@
 package message;
 
-import java.lang.String;
-
-public class InstantMessageService {
-
-    public String name;
-
-    public InstantMessageService(String name) {
-        this.name = name;
-    }
-
-    public void enviarMensagem() {
-        validarConexaoInternet();
-        System.out.println("Enviando mensagem pelo " + name);
-        salvarHistoricoMensagem();
-    }
-
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem pelo " + name);
-    }
-
-    private void validarConexaoInternet() {
-        System.out.println("Validando se está conectado à Internet");
-    }
-
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando mensagem no histórico");
-    }
+// Superclasse aplicando abstração!
+public abstract class InstantMessageService {
+    public abstract void enviarMensagem();
+    public abstract void receberMensagem();
 }
